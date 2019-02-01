ac04fbf6841f439be9142e1f5843ad36465e90cd
scream <hou.jennifer35@gmail.com>
2019-02-01 15:03:07 -0500

    new changes

--- render.js
+++ render.js
@@ -1,19 +1,27 @@
 const render = function (posters) {
     $('#display-poster').empty();
-    // let images = '';
+
     for (let i = 0; i < posters.length; i++) {
+
         const imgDiv = $('<img>');
         imgDiv.addClass('poster');
         if (posters[i].Poster !== 'N/A') {
             imgDiv.attr('src', posters[i].Poster);
             imgDiv.attr('imdb-id', posters[i].imdbID);
-
-            // images += `<img src='${posters[i].Poster}'>`;
+        }
+        else {
+            imgDiv.hide();
         }
         $('#display-poster').append(imgDiv);
     }
-    // $('#display-poster').append(images);
+
     $('#movie-title').val('');
     addListeners();
-
+    showPoster();
 }
+
+// const renderTrailer = function (key) {
+//     const src = `https://www.youtube.com/embed/${key}`;
+//     $('iframe').attr('src', src);
+//     showTrailer();
+// }
