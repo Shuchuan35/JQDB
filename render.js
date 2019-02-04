99d3212428723142f27c7ad69c408aee5a7202c2
scream <hou.jennifer35@gmail.com>
2019-02-04 10:00:47 -0500

    finished up dropdownmenu

--- render.js
+++ render.js
@@ -1,16 +1,20 @@
-const render = function (posters) {
+const render = function (posters, dropdown) {
     $('#display-poster').empty();
 
     for (let i = 0; i < posters.length; i++) {
-
         const imgDiv = $('<img>');
         imgDiv.addClass('poster');
-        if (posters[i].Poster !== 'N/A' && posters[i].Type === 'movie') {
-            imgDiv.attr('src', posters[i].Poster);
-            imgDiv.attr('imdb-id', posters[i].imdbID);
-        }
-        else {
-            imgDiv.hide();
+        if (dropdown == true) {
+            imgDiv.attr('src', `https://image.tmdb.org/t/p/w500${posters[i].poster_path}`);
+            imgDiv.attr('imdb-id', posters[i].id);
+        } else {
+            if (posters[i].Poster !== 'N/A' && posters[i].Type === 'movie') {
+                imgDiv.attr('src', posters[i].Poster);
+                imgDiv.attr('imdb-id', posters[i].imdbID);
+            }
+            else {
+                imgDiv.hide();
+            }
         }
         $('#display-poster').append(imgDiv);
     }
--- titlePoster.js
+++ titlePoster.js
@@ -1,4 +1,3 @@
-
 const getTitlePoster = function (title) {
     const queryURL = `https://www.omdbapi.com/?s=${title}&apikey=e0c3e966`;
     $.ajax({
@@ -7,7 +6,7 @@ const getTitlePoster = function (title) {
     }).then(function (response) {
         const titleA = Object.values(response);
         const posters = titleA[0];
-        render(posters);
+        render(posters, false);
     });
 }
 
@@ -23,10 +22,9 @@ const getTMDBID = function (imdbID, source) {
         const vKey = res.videos.results[0].key;
         const title = res.title;
         const brief = res.overview;
-        
+
         const imgDiv = $('<img>');
         imgDiv.attr('id', 'movie-poster');
-        // console.log(source);
         imgDiv.attr('src', source);
 
         const titleHolder = `<h5>${title}</h5>`;
@@ -35,19 +33,19 @@ const getTMDBID = function (imdbID, source) {
         $('#brief').append(titleHolder).append(briefHolder);
 
         const src = `https://www.youtube.com/embed/${vKey}`;
-        // $('#trailer-title').text(res.videos.results[0].name);
         $('iframe').attr('src', src);
         showTrailer();
     });
 }
 
-const getPopular = function (popularSearch) {
-    const popURL = `https://api.themoviedb.org/3/movie/popular?api_key=2404f28934c0e486a4e4a4accf9101c5&language=en-US&page=1`;
+const getPopular = function (search) {
+    const popURL = `https://api.themoviedb.org/3/movie/${search}?api_key=2404f28934c0e486a4e4a4accf9101c5&language=en-US&page=1`;
     $.ajax({
         url: popURL,
         method: 'GET'
-    })
-
-    render(posters);
+    }).then(function (response) {
+        const posters = response.results;
+        render(posters, true);
+    });
 }
 
